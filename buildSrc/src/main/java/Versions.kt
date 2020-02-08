749c290f404a6635a694f06e2f51c97978362e0c
Pushpal Roy <pushpal.roy@mutualmobile.com>
2020-02-09 03:17:14 +0530

    Initial commit

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,39 @@
+@Suppress("unused", "SpellCheckingInspection")
+object Versions {
+  private const val versionMajor = 1
+  private const val versionMinor = 0
+  private const val versionPatch = 0
+  private const val versionBuild = 0
+
+  const val compileSdk = 29
+  const val minSdk = 21
+  const val targetSdk = 29
+  const val versionCode = versionMajor * 10000 + versionMinor * 1000 + versionPatch * 100 + versionBuild
+  const val versionName = "$versionMajor.$versionMinor.$versionPatch.$versionBuild"
+
+  const val gradle = "3.5.3"
+  const val junit = "4.12"
+  const val androidx = "1.0.0"
+  const val accompat = "1.1.0-alpha05"
+  const val constraint = "1.1.3"
+  const val livedataKtx = "2.1.0-beta01"
+  const val material = "1.1.0-alpha01"
+  const val kotlin = "1.3.41"
+  const val archComponent = "2.0.0"
+  const val archCoreTesting = "1.1.1"
+  const val dagger = "2.24"
+  const val rxJava = "2.2.11"
+  const val rxAndroid = "2.1.1"
+  const val preferenceRoom = "1.1.8"
+  const val retrofit = "2.5.0"
+  const val gson = "2.8.5"
+  const val timber = "4.7.1"
+  const val memoryLeak = "2.0-beta-2"
+  const val glide = "4.9.0"
+  const val baseAdapter = "0.1.3"
+  const val androidVeil = "1.0.6"
+  const val mockito = "3.0.0"
+  const val mockitoKotlin = "2.1.0"
+  const val mockWebServer = "3.14.0"
+  const val espresso = "3.0.2"
+}
